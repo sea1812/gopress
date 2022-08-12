2aa6b67a9b908e0578667d5805cffbb2bc920594
sea1812 <1817813144@qq.com>
2022-08-12 23:09:48 +0800

    constructing first page

--- app/pages.go
+++ app/pages.go
@@ -1,7 +1,22 @@
 package app
 
-import "github.com/gogf/gf/net/ghttp"
+import (
+	"github.com/gogf/gf/frame/g"
+	"github.com/gogf/gf/net/ghttp"
+)
 
 func PageIndex(r *ghttp.Request) {
+	_ = r.Response.WriteTpl("fbase.html", g.Map{
+		//"app":           G_App_Setting,
+		"contentTpl": "front_index.html",
+		"bottomJs":   "front_index_js.html",
+		"activeband": "dashboard",
+		//"category"	:	 category,
+		//"catdict"	:	 catdict,
+		//"articles"	:	 articles,
+		//"page" 		:	 mpage,
+		//"pages"		:	 pagelist,
+		//"headlines"	:	 headlines,
+	})
 
 }
--- main.go
+++ main.go
@@ -7,7 +7,11 @@ import (
 
 func main() {
 	s := g.Server()
+	//绑定静态目录
+	s.AddStaticPath("/static", "./static")
+
 	GroupRoot := s.Group("/")
-	GroupRoot.GET("/", app.PageIndex)
+	GroupRoot.GET("/", app.PageIndex) //首页
+
 	s.Run()
 }
