b8248d9a93a8427cecd11662a75285c3e4f4a30d
sea1812 <1817813144@qq.com>
2022-08-12 22:46:45 +0800

    Begin coding

---
+++ app/pages.go
@@ -0,0 +1,7 @@
+package app
+
+import "github.com/gogf/gf/net/ghttp"
+
+func PageIndex(r *ghttp.Request) {
+
+}
--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import "github.com/gogf/gf/frame/g"
 
 func main() {
 	s := g.Server()
-
+	GroupRoot := s.Group("/")
+	GroupRoot.GET("/", app.PageIndex)
 	s.Run()
 }
